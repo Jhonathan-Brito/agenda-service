87081798736c725b7516c76c4806fa3ab400ecbe
Jhonathan Brito <gestorportuario.jb@gmail.com>
2022-05-16 22:09:58 -0300

    primeiro commit

---
+++ src/test/java/Jhonathan/com/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package Jhonathan.com.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
